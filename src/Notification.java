6a188caaeddd0676f81e3eb021fce1a892c7579f
Evan Coakley <evancoakley1@gmail.com>
2017-09-13 15:08:29 -0500

    initial commit

---
+++ src/Notification.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Notification {
+
+    public Notification() {
+        private LocalDateTime createdAt;
+        private String subject;
+        private String body;
+    }
+}
